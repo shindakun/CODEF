ea0b62fe59f80aacb775418e7552a7a638830010
Steve Layton <shindakun@users.noreply.github.com>
2017-06-16 16:26:35 -0700

    Continued...

--- codef/codef_gradient.js
+++ codef/codef_gradient.js
@@ -1,3 +1,5 @@
+'use strict';
+/*exported grad */
 /*------------------------------------------------------------------------------
 Copyright (c) 2011 Antoine Santo Aka NoNameNo
 
@@ -26,15 +28,15 @@ THE SOFTWARE.
 ------------------------------------------------------------------------------*/
 
 /**
-        <b>Create a new gradient object.</b><br>
-        grad(dst, params)<br>
-        @class grad
-        @param {Object} dst The destination canvas.
-        @param {Array} params An Array of the gradient definition.
-        @property {Object} The destination canvas.
-        @property {Array} params An Array of the gradient definition.
+  <b>Create a new gradient object.</b><br>
+  grad(dst, params)<br>
+  @class grad
+  @param {Object} dst The destination canvas.
+  @param {Array} params An Array of the gradient definition.
+  @property {Object} The destination canvas.
+  @property {Array} params An Array of the gradient definition.
 	@example
-	// gradient param is normalized, this example will declare 
+	// gradient param is normalized, this example will declare
 	// Black a start
 	// Blue at the 1st quarter
 	// Black at the half
@@ -42,59 +44,59 @@ THE SOFTWARE.
 	// then Black at the end
 
 	var mygradcolor=[{color: 'rgb(0,0,0)' ,  offset:0},
-		  	 {color: 'rgb(0,0,255)', offset:0.25},
-			 {color: 'rgb(0,0,0)' ,  offset:0.5},
-			 {color: 'rgb(0,0,255)', offset:0.75},
-			 {color: 'rgb(0,0,0)' ,  offset:1}
-			];
+  	 {color: 'rgb(0,0,255)', offset:0.25},
+		 {color: 'rgb(0,0,0)' ,  offset:0.5},
+		 {color: 'rgb(0,0,255)', offset:0.75},
+		 {color: 'rgb(0,0,0)' ,  offset:1}
+		];
 
 	var mygrad = new grad(mycanvas, mygradcolor);
 */
-function grad(dst,params){
-	this.dst=dst;
-	this.params=params;
-	
-	/**
-                <b>Draw the gradient Horizontally.</b><br>
-                grad.drawH()<br>
+function grad (dst, params) {
+	this.dst = dst;
+	this.params = params;
 
-                @function grad.drawH
-                @example
-                mygrad.drawH();
-        */
-	this.drawH = function(){
-		var tmp=this.dst.contex.fillStyle;
-		var tmp2=this.dst.contex.globalAlpha;
-		this.dst.contex.globalAlpha=1;
-		var lingrad = this.dst.contex.createLinearGradient(0,0,0,this.dst.canvas.height);
-		for(var j=0;j<this.params.length;j++){
+	/**
+    <b>Draw the gradient Horizontally.</b><br>
+    grad.drawH()<br>
+
+    @function grad.drawH
+    @example
+    mygrad.drawH();
+  */
+	this.drawH = function () {
+		var tmp = this.dst.contex.fillStyle;
+		var tmp2 = this.dst.contex.globalAlpha;
+		this.dst.contex.globalAlpha = 1;
+		var lingrad = this.dst.contex.createLinearGradient(0, 0, 0, this.dst.canvas.height);
+		for(let j = 0; j < this.params.length; j += 1){
 			lingrad.addColorStop(this.params[j].offset, this.params[j].color);
 		}
 		this.dst.contex.fillStyle = lingrad;
-		this.dst.contex.fillRect(0,0,this.dst.canvas.width,this.dst.canvas.height);
-		this.dst.contex.fillStyle=tmp;
-		this.dst.contex.globalAlpha=tmp2;
+		this.dst.contex.fillRect(0, 0, this.dst.canvas.width, this.dst.canvas.height);
+		this.dst.contex.fillStyle = tmp;
+		this.dst.contex.globalAlpha = tmp2;
 	}
-	
-	/**
-                <b>Draw the gradient Verticaly.</b><br>
-                grad.drawV()<br>
 
-                @function grad.drawV
-                @example
-                mygrad.drawV();
-        */
-	this.drawV = function(){
-		var tmp=this.dst.contex.fillStyle;
-		var tmp2=this.dst.contex.globalAlpha;
-		this.dst.contex.globalAlpha=1;
-		var lingrad = this.dst.contex.createLinearGradient(0,0,this.dst.canvas.width,0);
-		for(var j=0;j<this.params.length;j++){
+	/**
+    <b>Draw the gradient Verticaly.</b><br>
+    grad.drawV()<br>
+
+    @function grad.drawV
+    @example
+    mygrad.drawV();
+  */
+	this.drawV = function () {
+		var tmp = this.dst.contex.fillStyle;
+		var tmp2 = this.dst.contex.globalAlpha;
+		this.dst.contex.globalAlpha = 1;
+		var lingrad = this.dst.contex.createLinearGradient(0, 0, this.dst.canvas.width, 0);
+		for (let j = 0; j < this.params.length; j += 1){
 			lingrad.addColorStop(this.params[j].offset, this.params[j].color);
 		}
 		this.dst.contex.fillStyle = lingrad;
-		this.dst.contex.fillRect(0,0,this.dst.canvas.width,this.dst.canvas.height);
-		this.dst.contex.fillStyle=tmp;
-		this.dst.contex.globalAlpha=tmp2;
+		this.dst.contex.fillRect(0, 0, this.dst.canvas.width, this.dst.canvas.height);
+		this.dst.contex.fillStyle = tmp;
+		this.dst.contex.globalAlpha = tmp2;
 	}
 }
